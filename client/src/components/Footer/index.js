bd14156136a3cf8eed6b68d9371998197d170901
Justin Mozley <jtmozley@gmail.com>
2019-08-14 16:43:07 -0500

    Footer Edit
    Edited footer to display the github icon and upon click will open new tab to github repo

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 import "./style.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import background from "../Footer/background.jpeg";
 
-function Footer() {
+export default function Footer() {
   return (
-    <footer className="nav justify-content-center fixed-bottom">
-      <img src={background} alt=" insert alternative here "/>
-      <a className="nav-link" href="https://github.com/krayt0n3/rockclimbingappV2"><FontAwesomeIcon icon={faGithub} /></a>
-    </footer>
+    <div className="footer">
+      <i className="fab fa-react" />
+      <a
+        href="https://github.com/krayt0n3/rockclimbingappV2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-github" />
+        Repo
+      </a>
+    </div>
   );
 }
-
-export default Footer;
-
-// Rock and Ice
-// Climbing Porn
-// REI
-// MooseJaw
-// Back Country
-// Climb stuff
-// Auth0, Dark Sky, Rock & Ice, USG Water Services, Dark Sky, Google Maps, Mountain Project, GitHub, Heroku, JawsDB,   
-
-// when to go, weather, popular routes, average difficulty
