c71be2733d8708e93bb082fa908ade5bbf31019a
r8yt <hendersonkevin64@gmail.com>
2019-08-15 18:47:10 -0500

    font header changed

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -16,7 +16,9 @@ class FrontPage extends Component {
         Find new deals on gear consolidated into a single page.
         </div>
         <div className="col">
-      Test
+        Whether you are solving your long time project, or flashing a route for the first time, everytime you send a new route, you get to spend time in Sendsville. Sendsville is a magical place where you can take in the scenery around you and relish in the fact that you made it!
+Sendsville the app is a place to connect with climbers and learn about what is happening in the climbing world, see some motivational photos of climbing adventures, find some great articles to read about some amazing feats other climbers are pulling off, find some stellar new deals on gear you may need, and even find your next great crag and some awesome new projects.
+Sign up today so you can visit Sendsville from anything you can get on the web with!
         </div>
       </div>
       </>
